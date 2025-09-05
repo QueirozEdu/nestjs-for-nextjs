55e02be1cc7fa1e1e41a51eb3a8c9164d41f8d2a
Eduardo Queiroz <eduhmq@gmail.com>
2025-09-05 09:05:34 -0300

    add config for db

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,17 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { CustomParseIntPipe } from 'src/common/pipes/custom-parse-int-pipe.pipe';
 
 @Controller('user')
 export class UserController {
+  constructor(private readonly configService: ConfigService) {}
   @Get(':id')
   findOne(
     @Param('id', new CustomParseIntPipe())
     id: number,
   ) {
-    console.log(id, typeof id);
+    console.log(process.env.TEST || 'Default');
+    console.log(this.configService.getOrThrow('TEST1'));
     return `Hello from user ${id} controller `;
   }
 }
