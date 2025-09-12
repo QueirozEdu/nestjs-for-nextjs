51cecc110bd02e96f0dcbe5e75a7ddf931407c8e
Eduardo Queiroz <eduhmq@gmail.com>
2025-09-11 23:29:32 -0300

    habilitator add many to one relationship between post and user

--- src/post/dto/create-post.dto.ts
+++ src/post/dto/create-post.dto.ts
@@ -1,1 +1,25 @@
-export class CreatePostDto {}
+import {
+  IsString,
+  Length,
+  IsNotEmpty,
+  IsOptional,
+  IsUrl,
+} from 'class-validator';
+
+export class CreatePostDto {
+  @IsString({ message: 'Title must be a string' })
+  @Length(10, 150, { message: 'Title must be 10 to 150 characters long' })
+  title: string;
+
+  @IsString({ message: 'Excerpt must be a string' })
+  @Length(10, 200, { message: 'Excerpt must be 10 to 200 characters long' })
+  excerpt: string;
+
+  @IsString({ message: 'Content must be a string' })
+  @IsNotEmpty({ message: 'Content can not be empty' })
+  content: string;
+
+  @IsOptional()
+  @IsUrl({ require_tld: false })
+  coverImageUrl: string;
+}
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,26 +1,25 @@
+import { Repository } from 'typeorm';
 import { Injectable } from '@nestjs/common';
+import { Post } from './entities/post.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 import { CreatePostDto } from './dto/create-post.dto';
-import { UpdatePostDto } from './dto/update-post.dto';
+import { User } from 'src/user/entities/user.entity';
 
 @Injectable()
 export class PostService {
-  create(createPostDto: CreatePostDto) {
-    return 'This action adds a new post';
-  }
+  constructor(
+    @InjectRepository(Post)
+    private readonly postRepository: Repository<Post>,
+  ) {}
 
-  findAll() {
-    return `This action returns all post`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} post`;
-  }
-
-  update(id: number, updatePostDto: UpdatePostDto) {
-    return `This action updates a #${id} post`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} post`;
+  async create(dto: CreatePostDto, author: User) {
+    const post = this.postRepository.create({
+      title: dto.title,
+      excerpt: dto.excerpt,
+      content: dto.content,
+      author,
+    });
+    const created = await this.postRepository.save(post);
+    return created;
   }
 }
