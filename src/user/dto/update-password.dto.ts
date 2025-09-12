dac64fbe5466a02dcf4a4ca21c787074582b2c5b
Eduardo Queiroz <eduhmq@gmail.com>
2025-09-11 22:07:47 -0300

    fix typo

--- src/user/dto/update-password.dto.ts
+++ src/user/dto/update-password.dto.ts
@@ -1,11 +1,11 @@
 import { IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class UpdatePasswordDto {
-  @IsString({ message: 'Current password shoulb be a string' })
+  @IsString({ message: 'Current password should be a string' })
   @IsNotEmpty({ message: 'Current password should not be empty' })
   currentPassword: string;
 
-  @IsString({ message: 'New password shoulb be a string' })
+  @IsString({ message: 'New password should be a string' })
   @IsNotEmpty({ message: 'New password should not be empty' })
   @MinLength(6, { message: 'New password must be at least 6 characters long' })
   newPassword: string;
